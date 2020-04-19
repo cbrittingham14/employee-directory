4bd7183f805a61b5fd80a9ed5155dc8a5c7da594
chizz <cbrittingham@gmail.com>
2020-04-19 17:20:53 -0500

    got basic sort function working

--- employee-directory/src/components/Container.js
+++ employee-directory/src/components/Container.js
@@ -1,42 +1,84 @@
 import React, { Component } from 'react';
 import employees from '../employees.json';
 import Employee from './Employee';
+
 class Container extends Component {
 	text = '';
 	state = {
 		employees,
-		filter: ''
-	}
-	genKey = () =>{
-		return Math.random();
-	}
+		filter: '',
+		isSorted: false,
+		shouldSort: false
+	};
+
+	clearButton;
+
 	handleInputChange = e => {
-		
 		this.setState({
 			filter: e.target.value
 		});
-		console.log('filter ', this.state.filter);
-	};
-	handleSubmit = e => {
-		e.preventDefault();
-		this.setState({
-			filter:''
-		})
 	};
 
+	clearSort = () => {
+		this.setState({
+			isSorted: false,
+			shouldSort: false
+		});
+	};
+	setReverseSort = () =>{
+		this.clearButton = (<p />);
+		if(this.state.shouldSort){
+			this.clearButton = (
+				<button type='submit' onClick={this.clearSort} className='btn-warning'>
+				Clear Sort
+				</button>
+			);
+		}
+	};
+	sortCategory = e => {
+
+		e.preventDefault();
+		this.setState({
+			isSorted: !this.state.isSorted,
+			shouldSort: true
+		});
+
+		this.setReverseSort();
+	};
 	render() {
 		return(
 			<div>
-				<form className="form">
-				<input
-            value={this.state.filter}
-            name="filter"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Filter by Name"
-          />
-				</form>
-				<Employee emp={this.state.employees} filter={this.state.filter} />
+				<div className='row'>
+					<div className='col'>
+						<form className="form">
+						<input
+								value={this.state.filter}
+								name="filter"
+								onChange={this.handleInputChange}
+								type="text"
+								placeholder="Filter by Name"
+							/>
+						</form>
+					</div>
+					<div className='col'>
+						<button type="submit" onClick={this.sortCategory} className="btn btn-success">
+						Sort
+						</button>
+					</div>
+					<div className='col'>
+						{this.clearButton}
+					</div>
+				</div>
+				<div className='row'> 
+					<div className='col'>
+						<Employee 
+						emp={this.state.employees} 
+						filter={this.state.filter} 
+						isSorted={this.state.isSorted}
+						shouldSort={this.state.shouldSort}
+						/>
+						</div>
+				</div>
 			</div>
 		);
 	};
--- employee-directory/src/components/Employee.js
+++ employee-directory/src/components/Employee.js
@@ -2,20 +2,43 @@ import React from 'react';
 import TableHead from './TableHead';
 import Table from './Table';
 
-const Employee = (props) => {
+const Employee = ({ emp, filter, isSorted, shouldSort }) => {
 
-	const employees = props.emp;
+	const employees = emp;
 	let toRender;
+	let sorted;
+	let arrayToRender;
 
+	const compare = (a,b) => {
+		const nameA = a.department.toLowerCase();
+		const nameB = b.department.toLowerCase();
+		let comparison = 0;
+		if(nameA > nameB){
+			comparison = 1;
+		} else if (nameA < nameB){
+			comparison = -1;
+		}
+		return comparison;
+	}
+	
 	//returns an array of employees whos names match the filter text
 	const filtered = employees.filter(emp =>{
 		let first = emp.firstName.toLowerCase();
 		let last = emp.lastName.toLowerCase();
-		return (first.indexOf(props.filter) >= 0||last.indexOf(props.filter) >= 0);
+		return (first.indexOf(filter) >= 0||last.indexOf(filter) >= 0);
 	});
 
+	sorted = [...filtered].sort(compare);
+	if(shouldSort && !isSorted){
+		sorted.reverse();
+	}
+	arrayToRender = sorted;
+
+	if (!shouldSort){
+		arrayToRender = filtered;
+	}
 	//sets up the employee information to render in the view
-	toRender = filtered.map(i => 
+	toRender = arrayToRender.map(i => 
 		<tbody key={i.id}>
 			<tr>
 				<td>{i.id}</td>
@@ -26,7 +49,7 @@ const Employee = (props) => {
 			</tr>
 		</tbody>
 	);
-	
+
 	return (
 		<Table>
 			<TableHead />
